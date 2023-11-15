510f7634f2ebfe8ac4065568da8eacde8ddf2656
Hunganh3012 <80197990+Hunganh3012@users.noreply.github.com>
2023-11-15 14:52:28 +0700

    Gộp nhiều state lại thành 1 object ( xử lý state phức tạp)

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -4,22 +4,33 @@ import "./GameStyle.css";
 import { calculateWinner } from "../helper";
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(board);
+    // const [board, setBoard] = useState(Array(9).fill(null));
+    // const [xIsNext, setXIsNext] = useState(true);
+
+    const [state, setState]=useState(
+        {
+            board: Array(9).fill(null),
+            xIsNext: true
+        }
+    )
+    const winner = calculateWinner(state.board);
     const handleClick = (index) => {
-        const boardCopy = [...board];
+        const boardCopy = [...state.board];
         if (winner || boardCopy[index]) return;
-        boardCopy[index] = xIsNext ? "X" : "O";
-        setBoard(boardCopy);
-        setXIsNext(!xIsNext);
+        boardCopy[index] = state.xIsNext ? "X" : "O";
+        setState({
+            board:boardCopy,
+            xIsNext: !state.xIsNext
+        });
     };
     const ResetGame = () => {
-        setBoard(Array(9).fill(null));
+        setState({
+            board: Array(9).fill(null)
+        });
     };
     return (
         <div>
-            <Board cell={board} onClick={handleClick}></Board>
+            <Board cell={state.board} onClick={handleClick}></Board>
             {winner && <div className="game-winner"> Winner is {winner}</div>}
             <button className="game-reset" onClick={ResetGame}>
                 ResetGame
