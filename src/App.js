f9d9dd756620dd4eafb3d9a43a432f2beedd7041
Hunganh3012 <80197990+Hunganh3012@users.noreply.github.com>
2023-11-15 22:10:14 +0700

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-
-import './App.css';
-import Counter from './components/counter/Counter';
-import Game from './components/tictactoe/Game';
-import GameWithReducer from './components/tictactoe/GameWithReducer';
-import Toggle from './components/toggle/Toggle';
+import "./App.css";
+import Counter from "./components/counter/Counter";
+import Game from "./components/tictactoe/Game";
+import GameWithReducer from "./components/tictactoe/GameWithReducer";
+import Toggle from "./components/toggle/Toggle";
 
 function App() {
-  return (
-    <div>
-        <GameWithReducer></GameWithReducer>
-        {/* <Game></Game> */}
-        {/* <Toggle></Toggle> */}
-        {/* <Counter></Counter> */}
-    </div>
-  );
+    return (
+        <div>
+            <GameWithReducer></GameWithReducer>
+            {/* <Game></Game> */}
+            {/* <Toggle></Toggle> */}
+            {/* <Counter></Counter> */}
+        </div>
+    );
 }
 
 export default App;
